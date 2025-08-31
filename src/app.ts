0cb0a97ad1b6e1af5bbce40af3e1f55d6282cd52
Fernando <fernandopovinsky@hotmail.com>
2025-08-30 21:10:00 -0300

    Fix: Enhance logging for startup and route registration
    Added startup time logging and improved route registration logging.

--- src/app.ts
+++ src/app.ts
@@ -7,26 +7,35 @@ import { RoutesService } from "./modules";
 const port = process.env.PORT || "3000";
 
 export async function bootstrap() {
+  const startedAt = Date.now();
   const app = Fastify({ logger: true });
 
   app.setErrorHandler(errorInterceptor);
 
   app.addHook("onRoute", (routeOptions) => {
-    app.log.info(
-      `Route registered: ${routeOptions.method} ${routeOptions.url}`
-    );
+    if (routeOptions.method != "HEAD") {
+      app.log.info(
+        `Route registered: ${routeOptions.method} ${routeOptions.url}`
+      );
+    }
   });
+  app.log.info(`Initial setup done after ${Date.now() - startedAt}ms`);
 
   const mongoService = new MongoService(app);
 
   const routesService = new RoutesService();
   routesService.registerRoutes(app);
+  app.log.info(`Routes registered after ${Date.now() - startedAt}ms`);
 
   await mongoService.connect();
+  app.log.info(`MongoDB connected after ${Date.now() - startedAt}ms`);
 
   app
     .listen({ port: parseInt(port), host: "0.0.0.0" })
-    .then(() => app.log.info(`Server running on http://localhost:${port}`))
+    .then(() => {
+      app.log.info(`Server running on http://localhost:${port}`);
+      app.log.info(`Startup time: ${Date.now() - startedAt}ms`);
+    })
     .catch((err) => {
       app.log.error(err);
       process.exit(1);
